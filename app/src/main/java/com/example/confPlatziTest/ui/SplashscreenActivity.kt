2d8e8e56ce3c71ee9dc642b6830e9550b397397f
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-06-28 16:37:04 -0500

    pill

---
+++ app/src/main/java/com/example/confPlatziTest/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.confPlatziTest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.confPlatziTest.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
