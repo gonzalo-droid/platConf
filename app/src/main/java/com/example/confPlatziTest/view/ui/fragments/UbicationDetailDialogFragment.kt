6a83a322f314c799c333df3f49297a5308c72b47
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-07-07 22:00:12 -0500

    Splah

--- app/src/main/java/com/example/confPlatziTest/view/ui/fragments/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/example/confPlatziTest/view/ui/fragments/UbicationDetailDialogFragment.kt
@@ -1,18 +1,25 @@
 package com.example.confPlatziTest.view.ui.fragments
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.example.confPlatziTest.R
+import com.example.confPlatziTest.model.Ubication
+import kotlinx.android.synthetic.main.fragment_ubication_detail_dialog.*
 
-class UbicationDetailDialogFragment : Fragment() {
+class UbicationDetailDialogFragment : DialogFragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
     override fun onCreateView(
@@ -23,4 +30,44 @@ class UbicationDetailDialogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbarUbication.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolbarUbication.setTitleTextColor(Color.WHITE)
+        toolbarUbication.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val ubication = Ubication()
+        toolbarUbication.title = ubication.name
+        tvItemDetailUbicationTitle.text = ubication.name
+        tvItemDetailUbicationAddress.text = ubication.address
+        tvItemDetailUbicationPhone.text = ubication.phone
+        tvItemDetailUbicationLink.text = ubication.website
+
+        llTelehone.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${ubication.phone}")
+            }
+            startActivity(intent)
+        }
+        llLink.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(ubication.website)
+            startActivity(intent)
+        }
+
+        //estilos mpa
+        // https://mapstyle.withgoogle.com/
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+    }
 }
--- app/src/main/java/com/example/confPlatziTest/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/example/confPlatziTest/view/ui/fragments/UbicationFragment.kt
@@ -1,17 +1,28 @@
 package com.example.confPlatziTest.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.example.confPlatziTest.R
+import com.example.confPlatziTest.model.Ubication
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class UbicationFragment : Fragment(), OnMapReadyCallback {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +46,33 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
+    override fun onMapReady(googleMap: GoogleMap) {
+        val ubication = Ubication()
 
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitude, ubication.longitude)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom)) //centar mapa
+
+        val centerMark = LatLng(ubication.latitude, ubication.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("Platzi Cond 2020")
+
+        //cargar img en el mapa
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi)} as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150,150,false)
+
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        googleMap?.addMarker(markerOptions)
+
+        googleMap?.setOnMarkerClickListener(this)
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragmentDialog)
+        return true
     }
 
 }
